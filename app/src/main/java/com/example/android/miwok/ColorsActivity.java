efbed5ba8db143a69a8e2601ebc7718197daf9a4
Robin-Castellani <robin.castellani@studenti.unitn.it>
2020-03-14 11:59:42 +0100

    Add audio and play it in ColorsActivity.java

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.miwok;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -33,14 +34,14 @@ public class ColorsActivity extends AppCompatActivity {
         setContentView(R.layout.word_list);
 
         ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word("weṭeṭṭi", "red", R.drawable.color_red));
-        words.add(new Word("chokokki", "green", R.drawable.color_green));
-        words.add(new Word("ṭakaakki", "brown", R.drawable.color_brown));
-        words.add(new Word("ṭopoppi", "gray", R.drawable.color_gray));
-        words.add(new Word("kululli", "black", R.drawable.color_black));
-        words.add(new Word("kelelli", "white", R.drawable.color_white));
-        words.add(new Word("ṭopiisә", "dusty yellow", R.drawable.color_dusty_yellow));
-        words.add(new Word("chiwiiṭә", "mustard yellow", R.drawable.color_mustard_yellow));
+        words.add(new Word("weṭeṭṭi", "red", R.drawable.color_red, R.raw.color_red));
+        words.add(new Word("chokokki", "green", R.drawable.color_green, R.raw.color_green));
+        words.add(new Word("ṭakaakki", "brown", R.drawable.color_brown, R.raw.color_brown));
+        words.add(new Word("ṭopoppi", "gray", R.drawable.color_gray, R.raw.color_gray));
+        words.add(new Word("kululli", "black", R.drawable.color_black, R.raw.color_black));
+        words.add(new Word("kelelli", "white", R.drawable.color_white, R.raw.color_white));
+        words.add(new Word("ṭopiisә", "dusty yellow", R.drawable.color_dusty_yellow, R.raw.color_dusty_yellow));
+        words.add(new Word("chiwiiṭә", "mustard yellow", R.drawable.color_mustard_yellow, R.raw.color_mustard_yellow));
 
         // create an ArrayAdapter, useful to handle data in a RecyclerView
         // it puts every element of words into an Android standard View, the
@@ -51,27 +52,37 @@ public class ColorsActivity extends AppCompatActivity {
         );
 
         // get the id of the ListView
-        ListView listView = (ListView) findViewById(R.id.word_list);
+        final ListView listView = (ListView) findViewById(R.id.word_list);
 
         // attach the ArrayAdapter to the ListView
         listView.setAdapter(adapter);
 
+        // set the behaviour when a list item is clicked
         listView.setOnItemClickListener(
                 new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        Log.println(Log.INFO, "ITEMCLICK", "item clicked");
-                        Toast.makeText(getApplicationContext(), "Click performed", Toast.LENGTH_SHORT).show();
+                        // get the Word at the current position and get the pronunciation audio ID
+                        Word currentWord = (Word) listView.getItemAtPosition(position);
+                        Integer audioID = currentWord.getPronunciationResourceID();
+
+                        // if no audio, display a Toast and exit
+                        if (audioID == null) {
+                            Toast.makeText(
+                                    ColorsActivity.this,
+                                    "No pronunciation to play",
+                                    Toast.LENGTH_SHORT
+                            ).show();
+                            return;
+                        }
+
+                        // instantiate the MediaPlayer object and play the pronunciation
+                        MediaPlayer pronunciationAudio = MediaPlayer.create(
+                                getApplicationContext(), audioID
+                        );
+                        pronunciationAudio.start();
                     }
                 }
         );
-
-//        AdapterView.OnItemClickListener playMiwokClickListener = new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                Toast.makeText(getApplicationContext(), "Click performed", Toast.LENGTH_SHORT).show();
-//            }
-//        };
-//        listView.setOnItemClickListener(playMiwokClickListener);
     }
 }
